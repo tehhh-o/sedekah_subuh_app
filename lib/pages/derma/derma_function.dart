b18d79585896933eab8fcd28dc9c29b39e92fe07
tehhh-o <elvintehh@gmail.com>
2026-02-24 11:00:59 +0800

    Update derma_function.dart
    Changed 'Derma Tanpa Nama' text color to red

--- lib/pages/derma/derma_function.dart
+++ lib/pages/derma/derma_function.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Future<String?> showDermaActionSheet(BuildContext context, List<String> options) async {
+Future<String?> showDermaActionSheet(
+    BuildContext context, List<String> options) async {
   return await showCupertinoModalPopup<String>(
     context: context,
     builder: (BuildContext context) => CupertinoActionSheet(
-     
       actions: options
           .map(
             (text) => CupertinoActionSheetAction(
-              child: Text(text),
-               isDestructiveAction: false,
+              child: Text(
+                text,
+                style: TextStyle(
+                    color: text == 'Derma Tanpa Nama'
+                        ? const Color.fromARGB(255, 255, 95, 95)
+                        : null),
+              ),
+              isDestructiveAction: false,
               onPressed: () {
                 Navigator.pop(context, text);
               },
@@ -27,4 +33,3 @@ Future<String?> showDermaActionSheet(BuildContext context, List<String> options)
     ),
   );
 }
-
